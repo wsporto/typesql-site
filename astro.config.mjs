b3d2fac792b039d3f8b3f9bda3f23f3d3b379854
Wagner Porto <wsporto.dev@gmail.com>
2023-11-17 23:41:03 -0300

    Add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,42 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'TypeSQL',
-			social: {
-				github: 'https://github.com/wsporto/typesql',
-			},
-			sidebar: [
-				{
-					label: 'Overview',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Get started', link: '/guides/get-started/' },
-						{ label: 'Automatic CRUD generation', link: '/guides/crud/' },
-						{ label: 'Custom queries', link: '/guides/custom-queries/' },
-						{ label: 'Nested query result', link: '/guides/nested-query-result/' },
-					],
-				},
-				{
-					label: 'How-to Guides',
-					autogenerate: { directory: 'how-to' },
-				},
-			],
-			customCss: [
-				// Relative path to your custom CSS file
-				'./src/styles/custom.css',
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'TypeSQL',
+    social: {
+      github: 'https://github.com/wsporto/typesql'
+    },
+    sidebar: [{
+      label: 'Overview',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Get started',
+        link: '/guides/get-started/'
+      }, {
+        label: 'Automatic CRUD generation',
+        link: '/guides/crud/'
+      }, {
+        label: 'Custom queries',
+        link: '/guides/custom-queries/'
+      }, {
+        label: 'Nested query result',
+        link: '/guides/nested-query-result/'
+      }]
+    }, {
+      label: 'How-to Guides',
+      autogenerate: {
+        directory: 'how-to'
+      }
+    }],
+    customCss: [
+    // Relative path to your custom CSS file
+    './src/styles/custom.css']
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
